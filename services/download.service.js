8412d4d1090c6b59dd4fe70ada58550d9779a08e
Ranjeet Ghotra <ranjeetghotra6@gmail.com>
2020-07-29 18:23:56 +0530

    create zip from gridfs

--- services/download.service.js
+++ services/download.service.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const JsZip = require("jszip");
-const FileSaver = require('file-saver');
+const FileSaver = require("file-saver");
+const fs = require("fs");
+const archiver = require("archiver");
 const gfs = require("../config/gfs");
 const File = require("../models/file.model"); // File Model
 const config = require("../config/config");
+const { resolve } = require("path");
 // download file or Directory
 module.exports.file = async function (req, res, next) {
   try {
@@ -56,54 +59,53 @@ module.exports.file = async function (req, res, next) {
 // download file or Directory
 module.exports.files = async function (req, res, next) {
   try {
-    const zip = new JsZip();
-    var zipFile = zip.folder("download");
     var decoded = await jwt.verify(req.query.token, config.jwt.secret);
     if (decoded && Array.isArray(decoded.ids)) {
+      // create a file to stream archive data to.
+      var output = fs.createWriteStream(__dirname + "/example.zip");
+      var archive = archiver("zip", {
+        zlib: { level: 9 }, // Sets the compression level.
+      });
+
+      // pipe archive data to the file
+      archive.pipe(output);
       const ids = decoded.ids;
-      for (let i = 0; i < ids.length; i++) {
-        file = await File.findOne({ user: decoded.user, _id: ids[i] });
-        console.log(file);
-        if (file) {
-          const gfsFile = gfs.gfs
-            .find(new mongoose.Types.ObjectId(file.fileId))
-            .toArray((err, files) => {
-              if (!files || files.length === 0) {
-              } else {
-                let readableStream = gfs.gfs.openDownloadStreamByName(
-                  files[0].filename
-                );
-                var bufferArray = [];
-                readableStream.on("data", function (chunk) {
-                  bufferArray.push(chunk);
+      const promiseArray = [];
+      console.log(ids);
+      ids.forEach(id => {
+        promiseArray.push(
+          new Promise(async (resolve) => {
+            let file = await File.findOne({ user: decoded.user, _id: id });
+            if (file) {
+              const gfsFile = gfs.gfs
+                .find(new mongoose.Types.ObjectId(file.fileId))
+                .toArray((err, files) => {
+                  if (!files || files.length === 0) {
+                  } else {
+                    let readableStream = gfs.gfs.openDownloadStreamByName(
+                      files[0].filename
+                    );
+                    let bufferArray = [];
+                    readableStream.on("data", function (chunk) {
+                      bufferArray.push(chunk);
+                    });
+                    readableStream.on("end", function () {
+                      var buffer = Buffer.concat(bufferArray);
+                      archive.append(buffer, { name: file.originalName });
+                      resolve();
+                    });
+                  }
                 });
-                readableStream.on("end", function () {
-                  console.log('end');
-                  var buffer = Buffer.concat(bufferArray);
-                  zipFile.file(file.originalName, buffer);
-                  zip.generateAsync({ type: "nodebuffer" }).then((content) => {
-                    // res.send(content.toString('base64'));
-                    FileSaver.saveAs(content, "archive.zip");
-                  });
-                });
-              }
-            });
-        }
-      }
+            }
+          })
+        );
+      });
+      Promise.all(promiseArray).then(() => {
+        archive.finalize();
+        res.send({status: true, message: "Zip created"});
+      });
     }
   } catch (err) {
-    // err
     res.send({ status: false, message: err.message });
   }
 };
-
-/*
-async function multiDownloadAsZip(fileIds){
-  const zip = new JsZip();
-  const promiseArray = [];
-  fileIds.forEach(fileID => {
-    file = await File.findOne({ user: decoded.user, _id: ids[i] });
-    promiseArray.push()
-  });
-}
-*/
